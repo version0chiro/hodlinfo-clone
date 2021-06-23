f14bd59041dfca22b9c3585fddbd9ec2e993c932
Chiro <sachinsinghroxx@gmail.com>
2021-06-23 18:13:41 +0530

    added drop downs

---
+++ src/components/navigation/DropDownButtons.js
@@ -0,0 +1,36 @@
+import { DropdownButton, Dropdown, Button } from "react-bootstrap";
+
+const DropDownButtons = (props) => {
+  return (
+    <div className="drop-down-buttons">
+      <DropdownButton
+        variant={!props.darkMode ? "primary" : "secondary"}
+        id="dropdown-basic-button"
+        title="INR"
+      >
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </DropdownButton>
+      <DropdownButton
+        variant={!props.darkMode ? "primary" : "secondary"}
+        id="dropdown-basic-button"
+        title="BTC"
+      >
+        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+      </DropdownButton>
+      <a href="https://wazirx.com/invite/sp7pvbt6?utm_source=finstreet&utm_medium=affiliate&utm_campaign=regnow-btn">
+        <Button
+          variant={!props.darkMode ? "primary" : "secondary"}
+          id="dropdown-basic-button"
+        >
+          BUY BTC
+        </Button>
+      </a>
+    </div>
+  );
+};
+
+export default DropDownButtons;
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,9 +1,12 @@
 import DarkModeToggle from "./DarkModeToggle";
 import Logo from "./Logo";
+import DropDownButtons from "./DropDownButtons";
+
 const navigation = (props) => {
   return (
     <div className="navigation">
       <Logo />
+      <DropDownButtons darkMode={props.darkMode} />
       <DarkModeToggle
         setDarkMode={props.setDarkMode}
         darkMode={props.darkMode}
